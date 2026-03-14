5da0414170cae829f627f5bb5d0b2314cd3a28a2
IREStephen <g00465649@atu.ie>
2026-03-14 22:22:09 +0000

    Updated profile.html, profile.js, style.css

--- src/main/resources/static/profile.js
+++ src/main/resources/static/profile.js
@@ -6,51 +6,91 @@ window.onload = loadProfile;
 
 function loadProfile(){
 
-    fetch(`/api/users/profile/${userId}`)
-        .then(res=>res.json())
-        .then(data=>{
+fetch(`/api/users/profile/${userId}`)
+.then(res=>res.json())
+.then(data=>{
 
-            profileData = data;
+profileData = data;
 
-            document.getElementById("username").innerText = data.name;
-            document.getElementById("nameText").innerText = data.name;
-            document.getElementById("bioText").innerText = data.bio;
-            document.getElementById("avatarPreview").src = data.avatarUrl;
+document.getElementById("username").innerText = data.name;
+document.getElementById("nameText").innerText = data.name;
+document.getElementById("bioText").innerText = data.bio;
+document.getElementById("avatarPreview").src = data.avatarUrl;
+document.getElementById("miniAvatar").src = data.avatarUrl;
 
-        });
+});
+
 }
 
-function editField(field){
+function editName(){
 
-    let value = prompt("Enter new value:");
+let value = prompt("Enter new name:", profileData.name);
 
-    if(value){
-        profileData[field] = value;
-
-        if(field==="name")
-            document.getElementById("nameText").innerText=value;
-
-        if(field==="bio")
-            document.getElementById("bioText").innerText=value;
-
-        if(field==="avatarUrl")
-            document.getElementById("avatarPreview").src=value;
-    }
+if(value){
+profileData.name = value;
+document.getElementById("nameText").innerText = value;
+document.getElementById("username").innerText = value;
 }
+
+}
+
+function editBio(){
+
+let value = prompt("Enter new bio:", profileData.bio);
+
+if(value){
+profileData.bio = value;
+document.getElementById("bioText").innerText = value;
+}
+
+}
+
+function uploadAvatar(){
+
+document.getElementById("avatarInput").click();
+
+}
+
+document.getElementById("avatarInput").addEventListener("change", function(){
+
+const file = this.files[0];
+
+if(!file) return;
+
+const reader = new FileReader();
+
+reader.onload = function(e){
+
+profileData.avatarUrl = e.target.result;
+
+document.getElementById("avatarPreview").src = e.target.result;
+document.getElementById("miniAvatar").src = e.target.result;
+
+};
+
+reader.readAsDataURL(file);
+
+});
 
 function saveProfile(){
 
-    fetch(`/api/users/profile/${userId}`,{
+fetch(`/api/users/profile/${userId}`,{
 
-        method:"PUT",
+method:"PUT",
 
-        headers:{
-            "Content-Type":"application/json"
-        },
+headers:{
+"Content-Type":"application/json"
+},
 
-        body:JSON.stringify(profileData)
+body:JSON.stringify(profileData)
 
-    })
-        .then(()=>alert("Profile updated"));
+})
+.then(()=>{
+
+alert("Profile updated");
+
+location.reload();
+
+});
 
 }
